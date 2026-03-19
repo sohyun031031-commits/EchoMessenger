01a323f61c81c83f6af832f8ff032944faf3df19
sohyun031031-commits <sohyun031031@gmail.com>
2026-03-19 13:17:11 +0900

    과제2 코딩과 테스트 완료

--- Form1.cs
+++ Form1.cs
@@ -5,6 +5,7 @@ namespace EchoMessenger
         public Form1()
         {
             InitializeComponent();
+            this.AcceptButton = button1;
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
@@ -15,11 +16,21 @@ namespace EchoMessenger
         private void button1_Click(object sender, EventArgs e)
         {
             string typed_msg = textBox1.Text;
-            LstEchoWindow.Items.Add(typed_msg);
-            textBox1.Clear();
+
+            if (!string.IsNullOrWhiteSpace(typed_msg))
+            {
+                LstEchoWindow.Items.Add(typed_msg);
+                textBox1.Clear();
+                textBox1.Focus();
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
